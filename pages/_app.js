2e4ba9153a4fcaef00094ea545966f29a16304d8
robinatwork1999 <103563730+robinatwork1999@users.noreply.github.com>
2022-08-26 00:38:55 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,38 +1,17 @@
 import React from "react";
 import Header from "@/layout/header";
 import Footer from "@/layout/footer";
+import Head from "next/head";
 import NextNProgress from "nextjs-progressbar";
 import "../styles/globalLayout.scss";
-import Document, { Html, Head, Main, NextScript } from "next/document";
-import { readFileSync } from "fs";
-import { join } from "path";
-
-
-class InlineStylesHead extends Head {
-  getCssLinks = ({ allFiles }) => {
-    const { assetPrefix } = this.context;
-    if (!allFiles || allFiles.length === 0) return null;
-  
-    return allFiles
-      .filter((file) => /\.css$/.test(file))
-      .map((file) => (
-        <style
-          key={file}
-          nonce={this.props.nonce}
-          data-href={`${assetPrefix}/_next/${file}`}
-          dangerouslySetInnerHTML={{
-            __html: readFileSync(join(process.cwd(), ".next", file), "utf-8"),
-          }}
-        />
-      ));
-  };
-}
 
 function MyApp({ Component, pageProps }) {
   const { headerData = {}, footerData = {} } = pageProps;
   return (
     <React.Fragment>
-      <InlineStylesHead/>
+      <Head>
+        <meta name="description" content="Put your description here." />
+      </Head>
       <Header headerData={headerData} />
       <main className="main-content">
         <NextNProgress color="#103be6" />

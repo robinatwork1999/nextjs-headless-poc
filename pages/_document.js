4c440550c43391c3065e8944551b773af0d30994
robinatwork1999 <103563730+robinatwork1999@users.noreply.github.com>
2022-08-25 22:17:30 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,33 +2,32 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 import { readFileSync } from "fs";
 import { join } from "path";
 
-// class InlineStylesHead extends Head {
-//   getCssLinks = ({ allFiles }) => {
-//     const { assetPrefix } = this.context;
-//     if (!allFiles || allFiles.length === 0) return null;
-//     console.log(join(process.cwd(), ".next", allFiles[3]))
+class InlineStylesHead extends Head {
+  getCssLinks = ({ allFiles }) => {
+    const { assetPrefix } = this.context;
+    if (!allFiles || allFiles.length === 0) return null;
+    console.log(join(process.cwd(), ".next", allFiles[3]))
     
-//     return allFiles
-//       .filter((file) => /\.css$/.test(file))
-//       .map((file) => (
-//         <style
-//           key={file}
-//           nonce={this.props.nonce}
-//           data-href={`${assetPrefix}/_next/${file}`}
-//           dangerouslySetInnerHTML={{
-//             __html: readFileSync(join(process.cwd(), ".next", file), "utf-8"),
-//           }}
-//         />
-//       ));
-//   };
-// }
+    return allFiles
+      .filter((file) => /\.css$/.test(file))
+      .map((file) => (
+        <style
+          key={file}
+          nonce={this.props.nonce}
+          data-href={`${assetPrefix}/_next/${file}`}
+          dangerouslySetInnerHTML={{
+            __html: readFileSync(join(process.cwd(), ".next", file), "utf-8"),
+          }}
+        />
+      ));
+  };
+}
 
 export default class MainDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-        </Head>
+        <InlineStylesHead/>
         <body>
           <Main />
         </body>

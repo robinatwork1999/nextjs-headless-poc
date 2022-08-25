ed856c57e8980d1ba9c8ec2b3351ad827c99879d
robinatwork1999 <103563730+robinatwork1999@users.noreply.github.com>
2022-08-26 00:34:39 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,37 @@ import Footer from "@/layout/footer";
 import Head from "next/head";
 import NextNProgress from "nextjs-progressbar";
 import "../styles/globalLayout.scss";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { readFileSync } from "fs";
+import { join } from "path";
+import fs from "fs-extra";
+
+class InlineStylesHead extends Head {
+  getCssLinks = ({ allFiles }) => {
+    const { assetPrefix } = this.context;
+    if (!allFiles || allFiles.length === 0) return null;
+    if (!fs.existsSync(join(process.cwd(), ".next", allFiles[0]))) return null;
+  
+    return allFiles
+      .filter((file) => /\.css$/.test(file))
+      .map((file) => (
+        <style
+          key={file}
+          nonce={this.props.nonce}
+          data-href={`${assetPrefix}/_next/${file}`}
+          dangerouslySetInnerHTML={{
+            __html: readFileSync(join(process.cwd(), ".next", file), "utf-8"),
+          }}
+        />
+      ));
+  };
+}
 
 function MyApp({ Component, pageProps }) {
   const { headerData = {}, footerData = {} } = pageProps;
   return (
     <React.Fragment>
-      <Head>
-        <meta name="description" content="Put your description here." />
-      </Head>
+      <InlineStylesHead/>
       <Header headerData={headerData} />
       <main className="main-content">
         <NextNProgress color="#103be6" />

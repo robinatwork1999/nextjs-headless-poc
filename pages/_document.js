465f6c5d8e98fc646ac931ed999690c670a0d4e4
robinatwork1999 <103563730+robinatwork1999@users.noreply.github.com>
2022-08-25 23:25:06 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,32 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { readFileSync } from "fs";
 import { join } from "path";
+import { fs } from "fs-extra";
 
+class InlineStylesHead extends Head {
+  getCssLinks = ({ allFiles }) => {
+    const { assetPrefix } = this.context;
+    if (!allFiles || allFiles.length === 0) return null;
+    return allFiles
+      .filter((file) => /\.css$/.test(file))
+      .map((file) => (
+        <style
+          key={file}
+          nonce={this.props.nonce}
+          data-href={`${assetPrefix}/_next/${file}`}
+          dangerouslySetInnerHTML={{
+            __html: readFileSync(join(process.cwd(), ".next", file), "utf-8"),
+          }}
+        />
+      ));
+  };
+}
 
 export default class MainDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-        </Head>
+        <InlineStylesHead />
         <body>
           <Main />
         </body>
